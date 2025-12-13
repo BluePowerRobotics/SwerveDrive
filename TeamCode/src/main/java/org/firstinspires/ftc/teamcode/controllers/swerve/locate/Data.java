5887540f93d5bed3368cf5e1425633551f9a96fb
WilliamGao1130 <134742060+WilliamGao1130@users.noreply.github.com>
2025-12-13 12:11:37 +0800

    加入定位程序

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/swerve/locate/Data.java
@@ -0,0 +1,67 @@
+package org.firstinspires.ftc.teamcode.controllers.swerve.locate;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.utility.MathSolver;
+import org.firstinspires.ftc.teamcode.utility.Point2D;
+@Config
+public class Data {
+    private Data(){}
+    @Override
+    public String toString(){
+        return "Position:\nx:"+getPosition(DistanceUnit.MM).getX()+
+                "\ny:"+getPosition(DistanceUnit.MM).getY()+
+                "\nHeadingRadian:\n"+headingRadian+
+                "\nHeadingSpeedRadianPerSec:\n"+headingSpeedRadianPerSec+
+                "\nSpeed:\nX:"+getSpeed(DistanceUnit.MM).x+
+                "\nY:"+getSpeed(DistanceUnit.MM).y;
+    }
+    static Data instance=new Data();
+    public static Data getInstance(){return instance;}
+    private Point2D position=new Point2D(0,0);
+    public void setPosition(Point2D positionInch){
+        this.position=new Point2D(positionInch);
+    }
+    public Point2D getPosition(DistanceUnit distanceUnit){
+        switch (distanceUnit) {
+            case METER:
+                return new Point2D(MathSolver.toMM(position.getX())/1000,MathSolver.toMM(position.getY())/1000);
+            case CM:
+                return new Point2D(MathSolver.toMM(position.getX())/10,MathSolver.toMM(position.getY())/10);
+            case MM:
+                return new Point2D(MathSolver.toMM(position.getX()),MathSolver.toMM(position.getY()));
+            case INCH:
+                return position;
+        }
+        return null;
+    }
+    public double headingRadian=0;
+    public double headingSpeedRadianPerSec=0;
+    private Vector2d speed=new Vector2d(0,0);
+    public void setSpeed(Vector2d speed){
+        this.speed = speed;
+    }
+    public Vector2d getSpeed(DistanceUnit distanceUnit){
+        switch (distanceUnit) {
+            case METER:
+                return new Vector2d(MathSolver.toMM(speed.x)/1000,MathSolver.toMM(speed.y)/1000);
+            case CM:
+                return new Vector2d(MathSolver.toMM(speed.x)/10,MathSolver.toMM(speed.y)/10);
+            case MM:
+                return new Vector2d(MathSolver.toMM(speed.x),MathSolver.toMM(speed.y));
+            case INCH:
+                return speed;
+        }
+        return null;
+    }
+    public Pose2d getPose2d(){
+        return new Pose2d(getPosition(DistanceUnit.INCH).getY(),-getPosition(DistanceUnit.INCH).getX(),headingRadian);
+    }
+    public void setPose2d(Pose2d pose2d){
+        setPosition(new Point2D(-pose2d.position.y,+pose2d.position.x));
+        headingRadian=pose2d.heading.toDouble();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/swerve/locate/RobotPosition.java
@@ -0,0 +1,98 @@
+package org.firstinspires.ftc.teamcode.controllers.swerve.locate;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.RoadRunner.Localizer;
+import org.firstinspires.ftc.teamcode.RoadRunner.PinpointLocalizer;
+import org.firstinspires.ftc.teamcode.utility.Point2D;
+@Config
+public class RobotPosition {
+    public static class Params {
+        static double inPerTick = 0.001999;
+        static int minUpdateIntervalMs = 1; // 最小更新时间间隔，单位毫秒
+
+    }
+    public void setMinUpdateIntervalMs(@NonNull int interval){
+        Params.minUpdateIntervalMs=interval;
+    }
+
+    private static RobotPosition instance;
+    public static RobotPosition getInstance(){
+        if(instance==null){
+            throw new IllegalStateException("RobotPosition not initialized, call setInstance first");
+        }
+        return instance;
+    }
+    /**
+    * 初始化位置(全新)
+    * @param hardwareMap 硬件映射
+     * @param initialPosition 初始位置(inch)
+     * @param initialHeadingRadian 初始朝向，弧度制
+    * @return RobotPosition实例
+     */
+    public static RobotPosition refresh(HardwareMap hardwareMap,Point2D initialPosition,double initialHeadingRadian){
+        instance=new RobotPosition(hardwareMap);
+        instance.initialPosition=initialPosition;
+        instance.initialHeadingRadian=initialHeadingRadian;
+        Data.instance.setPosition(initialPosition);
+        Data.instance.headingRadian=initialHeadingRadian;
+        instance.localizer = new PinpointLocalizer(hardwareMap,Params.inPerTick, new Pose2d(-Data.instance.getPosition(DistanceUnit.INCH).getY(), +Data.instance.getPosition(DistanceUnit.INCH).getX(), Data.instance.headingRadian));
+        return instance;
+    }
+    /**
+     * 初始化位置(全新)
+     * @param hardwareMap 硬件映射
+     * @param pose2d 初始位置(roadrunner方向)
+     * @return RobotPosition实例
+     */
+    public static RobotPosition refresh(HardwareMap hardwareMap, Pose2d pose2d) {
+        return refresh(hardwareMap, new Point2D(-pose2d.position.y, pose2d.position.x), pose2d.heading.log());
+    }
+    /**
+     * 初始化位置(旧位置)
+     * @param hardwareMap 硬件映射
+     * @return RobotPosition实例
+     */
+    public static RobotPosition refresh(HardwareMap hardwareMap){
+        instance=new RobotPosition(hardwareMap);
+        instance.initialPosition= Data.instance.getPosition(DistanceUnit.INCH);
+        instance.initialHeadingRadian= Data.instance.headingRadian;
+        instance.localizer = new PinpointLocalizer(hardwareMap, Params.inPerTick,Data.getInstance().getPose2d());
+        return instance;
+    }
+
+    private RobotPosition(HardwareMap hardwareMap){
+        this.hardwareMap=hardwareMap;
+        }
+
+    private HardwareMap hardwareMap;
+    public Localizer localizer;
+    public Point2D initialPosition=new Point2D(0,0);
+    public double initialHeadingRadian=0;
+
+    public long lastUpdateTime=0;
+
+    public void update(){
+        if(System.currentTimeMillis()-lastUpdateTime<Params.minUpdateIntervalMs){
+            return;
+        }
+        PoseVelocity2d poseVelocity2d = localizer.update();
+        Pose2d pose = localizer.getPose();
+        Data.instance.headingSpeedRadianPerSec=poseVelocity2d.angVel;
+        Data.instance.setSpeed(new Vector2d(-poseVelocity2d.linearVel.y,+poseVelocity2d.linearVel.x));
+        Data.instance.setPosition(new Point2D(-pose.position.y,+pose.position.x));
+        Data.instance.headingRadian=pose.heading.log();
+        lastUpdateTime=System.currentTimeMillis();
+    }
+    public Data getData(){
+        update();
+        return Data.instance;
+    }
+}
