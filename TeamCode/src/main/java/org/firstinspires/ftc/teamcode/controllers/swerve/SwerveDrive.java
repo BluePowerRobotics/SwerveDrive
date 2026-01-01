5b262ca2cabd36a884a9781a64de7ec1359e295b
WilliamGao1130 <134742060+WilliamGao1130@users.noreply.github.com>
2026-01-01 22:50:20 +0800

    硬件命名采用惯例模式/use traditional name style 在同轴轮中加入角度传感器/add angleSensor requirement in coaxial wheel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/swerve/SwerveDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/swerve/SwerveDrive.java
@@ -5,30 +5,43 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.controllers.AngleSensor;
 import org.firstinspires.ftc.teamcode.controllers.swerve.wheelunit.ServoCoaxialWheel;
 import org.firstinspires.ftc.teamcode.controllers.swerve.wheelunit.ServoCoaxialWheelConfig;
 import org.firstinspires.ftc.teamcode.utility.Point2D;
 @Config
 public class SwerveDrive {
-    public static ServoCoaxialWheelConfig frontLeft = new ServoCoaxialWheelConfig(new Point2D(-1,1),1,0);
-    public static ServoCoaxialWheelConfig frontRight = new ServoCoaxialWheelConfig(new Point2D(1,1),1,0);
-    public static ServoCoaxialWheelConfig backLeft = new ServoCoaxialWheelConfig(new Point2D(-1,-1),1,0);
-    public static ServoCoaxialWheelConfig backRight = new ServoCoaxialWheelConfig(new Point2D(1,-1),1,0);
+    public static ServoCoaxialWheelConfig leftFront = new ServoCoaxialWheelConfig(new Point2D(-1,1),1,0);
+    public static ServoCoaxialWheelConfig rightFront = new ServoCoaxialWheelConfig(new Point2D(1,1),1,0);
+    public static ServoCoaxialWheelConfig leftBack = new ServoCoaxialWheelConfig(new Point2D(-1,-1),1,0);
+    public static ServoCoaxialWheelConfig rightBack = new ServoCoaxialWheelConfig(new Point2D(1,-1),1,0);
     public SwerveController swerveController;
     public SwerveDrive(HardwareMap hardwareMap){
         swerveController = new SwerveController(hardwareMap,
-                new ServoCoaxialWheel(frontLeft,
-                        hardwareMap.get(DcMotorEx.class,"frontLeft"),
-                        hardwareMap.get(Servo.class,"frontLeft")),
-                new ServoCoaxialWheel(frontRight,
-                        hardwareMap.get(DcMotorEx.class,"frontRight"),
-                        hardwareMap.get(Servo.class,"frontRight")),
-                new ServoCoaxialWheel(backLeft,
-                        hardwareMap.get(DcMotorEx.class,"backLeft"),
-                        hardwareMap.get(Servo.class,"backLeft")),
-                new ServoCoaxialWheel(backRight,
-                        hardwareMap.get(DcMotorEx.class,"backRight"),
-                        hardwareMap.get(Servo.class,"backRight"))
+                new ServoCoaxialWheel(leftFront,
+                        hardwareMap.get(DcMotorEx.class,"leftFront"),
+                        hardwareMap.get(Servo.class,"leftFront"),
+                        new AngleSensor(
+                                hardwareMap,"leftFront",0
+                        )),
+                new ServoCoaxialWheel(rightFront,
+                        hardwareMap.get(DcMotorEx.class,"rightFront"),
+                        hardwareMap.get(Servo.class,"rightFront"),
+                        new AngleSensor(
+                                hardwareMap,"rightFront",0
+                        )),
+                new ServoCoaxialWheel(leftBack,
+                        hardwareMap.get(DcMotorEx.class,"leftBack"),
+                        hardwareMap.get(Servo.class,"leftBack"),
+                        new AngleSensor(
+                                hardwareMap,"leftBack",0
+                        )),
+                new ServoCoaxialWheel(rightBack,
+                        hardwareMap.get(DcMotorEx.class,"rightBack"),
+                        hardwareMap.get(Servo.class,"rightBack"),
+                        new AngleSensor(
+                                hardwareMap,"rightBack",0
+                        ))
         );
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/swerve/wheelunit/ServoCoaxialWheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/swerve/wheelunit/ServoCoaxialWheel.java
@@ -3,22 +3,25 @@ package org.firstinspires.ftc.teamcode.controllers.swerve.wheelunit;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.controllers.AngleSensor;
 import org.firstinspires.ftc.teamcode.utility.Point2D;
 
 public class ServoCoaxialWheel implements WheelUnit{
     ServoCoaxialWheelConfig config;
     DcMotorEx motor;
     Servo servo;
+    AngleSensor angleSensor;
 
     @Override
     public Point2D getPosition() {
         return null;
     }
 
-    public ServoCoaxialWheel(ServoCoaxialWheelConfig servoCoaxialWheelConfig, DcMotorEx dcMotor, Servo servo){
+    public ServoCoaxialWheel(ServoCoaxialWheelConfig servoCoaxialWheelConfig, DcMotorEx dcMotor, Servo servo, AngleSensor angleSensor){
         this.config = servoCoaxialWheelConfig;
         this.motor = dcMotor;
         this.servo = servo;
+        this.angleSensor=angleSensor;
     }
     @Override
     public void setSpeed(double speed) {
