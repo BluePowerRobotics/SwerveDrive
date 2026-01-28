8471afc6cd03f6a837c61704fd1a55ac8df14c3f
WilliamGao1130 <134742060+WilliamGao1130@users.noreply.github.com>
2026-01-28 17:45:12 +0800

    修改SVATuning的输出方式/improve SVATuning outputting method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerve/SVATuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerve/SVATuning.java
@@ -17,41 +17,64 @@ import java.util.List;
 @TeleOp
 public class SVATuning extends LinearOpMode {
     SwerveDrive swerveDrive;
+    enum TuningMode{
+        ROTATION, TRANSLATION;
+        TuningMode next(){
+            return this==ROTATION?TRANSLATION:ROTATION;
+        }
+    }
+    TuningMode tuningMode = TuningMode.ROTATION;
     List<List<Point2D>> point2DSs;
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = InstanceTelemetry.init(telemetry);
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         swerveDrive = new SwerveDrive(hardwareMap);
-        waitForStart();
-        swerveDrive.swerveController.gamepadInput(0,0,0.1);
-        boolean inited=false;
-        while (!inited) {
-            boolean moved = true;
-            for (WheelUnit wheelUnit : swerveDrive.swerveController.wheelUnits) {
-                if (Math.abs(Point2D.dot(Point2D.fromPolar(wheelUnit.getHeading(), 1), wheelUnit.getPosition())) < 0.01) {
-                    moved = false;
-                }
+        while (opModeInInit()) {
+            if (tuningMode == TuningMode.ROTATION) {
+                telemetry.addLine("Tuning Mode: Rotation");
+            } else {
+                telemetry.addLine("Tuning Mode: Translation");
             }
-            if (moved) {
-                inited = true;
-            }
-            swerveDrive.swerveController.gamepadInput(0,0,0.1);
-        }
-        DcMotorEx[] dcMotorExes = new DcMotorEx[SwerveDrive.PARAMS.unitNames.length];
-        double[] power=new double[SwerveDrive.PARAMS.unitNames.length];
-        double[] velocity=new double[SwerveDrive.PARAMS.unitNames.length];
-        for (int i = 0; i < SwerveDrive.PARAMS.unitNames.length; i++) {
-            dcMotorExes[i] = hardwareMap.get(DcMotorEx.class, SwerveDrive.PARAMS.unitNames[i]);
-            power[i]=0;
-            velocity[i]=0;
-        }
-        while(opModeIsActive()){
-            for(int i=0;i<SwerveDrive.PARAMS.unitNames.length;i++) {
-                telemetry.addData("Motor"+i+"Power", power[i]);
-                telemetry.addData("Motor Velocity", velocity[i]);
+            if (gamepad1.aWasReleased()) {
+                tuningMode = tuningMode.next();
             }
             telemetry.update();
+        }
+        switch (tuningMode) {
+            case ROTATION:
+            waitForStart();
+            swerveDrive.swerveController.gamepadInput(0, 0, 0.1);
+            boolean initiated = false;
+            while (!initiated) {
+                boolean moved = true;
+                for (WheelUnit wheelUnit : swerveDrive.swerveController.wheelUnits) {
+                    if (Math.abs(Point2D.dot(Point2D.fromPolar(wheelUnit.getHeading(), 1), wheelUnit.getPosition())) < 0.01) {
+                        moved = false;
+                    }
+                }
+                if (moved) {
+                    initiated = true;
+                }
+                swerveDrive.swerveController.gamepadInput(0, 0, 0.1);
+            }
+            double[] power = new double[SwerveDrive.PARAMS.unitNames.length];
+            double[] velocity = new double[SwerveDrive.PARAMS.unitNames.length];
+            for (int i = 0; i < SwerveDrive.PARAMS.unitNames.length; i++) {
+                power[i] = 0;
+                velocity[i] = 0;
+            }
+            while (opModeIsActive()) {
+                for (int i = 0; i < SwerveDrive.PARAMS.unitNames.length; i++) {
+                    swerveDrive.swerveController.wheelUnits[i].setPower(power[i]);
+                    swerveDrive.swerveController.wheelUnits[i].update();
+                    telemetry.addData("Motor" + SwerveDrive.PARAMS.unitNames[i] + "Power", power[i]);
+                    telemetry.addData("Motor" +SwerveDrive.PARAMS.unitNames[i]+"Velocity", velocity[i]);
+                }
+                telemetry.update();
+            }
+            break;
+            case TRANSLATION:
         }
     }
 }
