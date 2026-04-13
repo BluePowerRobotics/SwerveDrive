8e43170d4d23b47e811f67d164f7f6d9b9533046
WilliamGao1130 <gaoyiwei1130@outlook.com>
2026-04-13 17:53:52 +0800

    增强参数/improve params

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/swerve/SwerveDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/swerve/SwerveDrive.java
@@ -102,7 +102,7 @@ public class SwerveDrive {
             0, Servo.Direction.REVERSE, ServoCoaxialWheelConfig.AngleSenSorDirection.FORWARD,
             13.7, 32.0/105.0, 54.0/20.0, 2.5);
     public static ServoCoaxialWheelConfig leftBack = new ServoCoaxialWheelConfig(new Point2D(-6.732283,-4.76378),
-            -0.256, Servo.Direction.REVERSE, ServoCoaxialWheelConfig.AngleSenSorDirection.FORWARD,
+            -0.156, Servo.Direction.REVERSE, ServoCoaxialWheelConfig.AngleSenSorDirection.FORWARD,
             -13.7, 32.0/105.0, 54.0/20.0, 2.5);
     //TODO REVERSEMOTOR
     public static ServoCoaxialWheelConfig rightBack = new ServoCoaxialWheelConfig(new Point2D(6.732283,-4.76378),
@@ -111,7 +111,7 @@ public class SwerveDrive {
     public static ServoCoaxialWheel.Params leftFrontParams = new ServoCoaxialWheel.Params()
             .withSP(0.6).withSI(0).withSD(0)
             .withMP(0.0).withMI(0).withMD(0.0)
-            .withKS(0).withKV(0).withKA(0)
+            .withKS(1.0181325156302465).withKV(0.004443769305569951).withKA(0)
             .withKM(0).withKJ(0);
     public static ServoCoaxialWheel.Params rightFrontParams = new ServoCoaxialWheel.Params()
             .withSP(0.6).withSI(0).withSD(0)
@@ -121,12 +121,12 @@ public class SwerveDrive {
     public static ServoCoaxialWheel.Params leftBackParams = new ServoCoaxialWheel.Params()
             .withSP(0.6).withSI(0).withSD(0)
             .withMP(0.0).withMI(0).withMD(0.0)
-            .withKS(0).withKV(0).withKA(0)
+            .withKS(0.736480508780421).withKV(0.0046527144341508295).withKA(0)
             .withKM(0).withKJ(0);
     public static ServoCoaxialWheel.Params rightBackParams = new ServoCoaxialWheel.Params()
             .withSP(0.6).withSI(0).withSD(0)
             .withMP(0.0).withMI(0).withMD(0.0)
-            .withKS( 0.639257338764168).withKV(0.0041913862299546335).withKA(0)
+            .withKS(0.639257338764168).withKV(0.0041913862299546335).withKA(0)
             .withKM(0).withKJ(0);
     public SwerveController swerveController;
     public final LazyImu lazyImu;
